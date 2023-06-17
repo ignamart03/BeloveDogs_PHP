72c98f389fe1553a90c472a0e0a3b88c8aabeecb
ignamart03 <90852344+ignamart03@users.noreply.github.com>
2023-06-17 14:00:09 +0200

    changes fixed navbar

--- register.php
+++ register.php
@@ -8,7 +8,6 @@
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="styles/base.css">
-
 </head>
 
 <body>
