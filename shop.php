7faa91c06f6a7106965d877de4202fb09672a515
ignamart03 <90852344+ignamart03@users.noreply.github.com>
2023-06-17 12:55:12 +0200

    Update shop.php

--- shop.php
+++ shop.php
@@ -8,7 +8,6 @@
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="styles/base.css">
-    <link rel="stylesheet" href="styles/shop.css">
 </head>
 
 <body>
@@ -43,135 +42,23 @@
         </div>
     </nav>
 
-    <div class="container">
-    <div class="sidebar">
-      <h2>Categorias</h2>
-      <select id="category-dropdown">
-        <option value="all">Todos los artículos</option>
-        <option value="perros">Para perros</option>
-        <option value="gatos">Para gatos</option>
-      </select>
+    <div class="container mt-4">
+        <h2 class="mb-4">Tienda</h2>
+        <div class="row">
+            <!-- Usaremos el ciclo for para simplificar la generación de las imágenes en la grilla -->
+            <?php for ($i=1; $i <= 16; $i++): ?>
+                <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+                    <div class="card">
+                        <img src="images/<?php echo $i; ?>.jpg" class="card-img-top" alt="Imagen <?php echo $i; ?>">
+                        <div class="card-body">
+                            <h5 class="card-title">Imagen <?php echo $i; ?></h5>
+                            <a href="#" class="btn btn-primary">Ir a producto</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endfor; ?>
+        </div>
     </div>
-  
-    <div class="content">
-      <h2>Tienda</h2>
-      <div class="image-container">
-
-        <div class="row">
-
-          <div class="parent">
-            <div class="child img_1">
-              <a id="img_txt" href="https://www.kiwoko.com/perros/accesorios-para-perros/juguetes-para-perros/summer-vibes-piscina-para-perros/SMV58901_M.html" target="_blank">Piscina para <br><br> perros</a>
-            </div>
-          </div>
-
-          <div class="parent">
-            <div class="child img_2">
-              <a id="img_txt" href="https://www.kiwoko.com/perros/accesorios-para-perros/julius-k9-idc-arnes-ergonomigo-negro-para-perros/JULJ16779_M.html" target="_blank">Arnes K9</a>
-            </div>
-          </div>
-
-          <div class="parent">
-            <div class="child img_3">
-              <a id="img_txt" href="https://www.kiwoko.com/peluches/fluffy-erizo-de-peluche-para-perros/FLF70448_M.html" target="_blank">Fluffy Erizo</a>
-            </div>
-          </div>
-
-          <div class="parent">
-            <div class="child img_4">
-              <a id="img_txt" href="https://www.kiwoko.com/peluches/fluffy-canvas-cocodrilo-de-peluche-para-perros/FLF16447_M.html" target="_blank">Fluffy Cocodrilo</a>
-            </div>
-          </div>
-
-        </div>
-  
-        <div class="row">
-
-          <div class="parent">
-            <div class="child img_5">
-              <a id="img_txt" href="https://www.kiwoko.com/gatos/comida-para-gatos/maltas-y-hierbas/malta/criadores-malta-omega-3-y-6-para-gatos-y-perros/CRD32370_M.html" target="_blank">Malta Omega 3 y 6</a>
-            </div>
-          </div>
-
-          <div class="parent">
-            <div class="child img_6">
-              <a id="img_txt" href="https://www.kiwoko.com/gatos/higiene-para-gatos/areneros-y-bandejas/nova-clean-beta-toillete-arenero-para-gatos/NCL70697_M.html" target="_blank">Nova Clean Beta</a>
-            </div>
-          </div>
-
-          <div class="parent">
-            <div class="child img_7">
-              <a id="img_txt" href="https://www.elcorteingles.es/mascotas/A18063756-pienso-para-gatos-esterilizados-senior-ultima-senior-10-pollo/?parentCategoryId=999.20999297013" target="_blank">Ultima Esterilizados <br><br> (Pollo)</a>
-            </div>
-          </div>
-
-          <div class="parent">
-            <div class="child img_8">
-              <a id="img_txt" href="https://www.tiendanimal.es/kong-snake-teaser-peluche-para-gatos/KONJU03366_M.html" target="_blank">Kong Snake Teaser</a>
-            </div>
-          </div>
-
-        </div>
-
-        <div class="row">
-
-          <div class="parent">
-            <div class="child img_9">
-              <a id="img_txt" href="https://www.miscota.es/perros/trixie/gulliver-transport-n-4-52x51x72-cm-gris-cl-gris-16100" target="_blank">Transportín para <br><br> coche</a>
-            </div>
-          </div>
-
-          <div class="parent">
-            <div class="child img_10">
-              <a id="img_txt" href="https://www.miscota.es/perros/vectra/pr-171992" target="_blank">Antiparásitos <br><br> Vectra</a>
-            </div>
-          </div>
-
-          <div class="parent">
-            <div class="child img_11">
-              <a id="img_txt" href="https://www.tiendanimal.es/salvaje-base-adultos-pollo-pienso-para-perros/SAB0991_M.html" target="_blank">Salvaje (Pollo)</a>
-            </div>
-          </div>
-
-          <div class="parent">
-            <div class="child img_12">
-              <a id="img_txt" href="https://www.kiwoko.com/perros/comida-para-perros/pienso-seco-para-perros/pienso-natural/pienso-true-origins-pure-salmon-para-perros/TRU70969_M.html" target="_blank">True Origins <br><br> (Salmón)</a>
-            </div>
-          </div>
-
-        </div>
-
-        <div class="row">
-
-          <div class="parent">
-            <div class="child img_13">
-              <a id="img_txt" href="https://www.miscota.es/gatos/seresto/seresto-collar-32599" target="_blank">Seresto <br><br> antiparásitos</a>
-            </div>
-          </div>
-
-          <div class="parent">
-            <div class="child img_14">
-              <a id="img_txt" href="" target="_blank"></a>
-            </div>
-          </div>
-
-          <div class="parent">
-            <div class="child img_15">
-              <a id="img_txt" href="" target="_blank"></a>
-            </div>
-          </div>
-
-          <div class="parent">
-            <div class="child img_16">
-              <a id="img_txt" href="" target="_blank"></a>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </div>
-  </div>
 </body>
 
 </html>
