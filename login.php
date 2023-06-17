7e408beb17555ebeb190615cc3bc2ccf1f5dbdf2
ignamart03 <90852344+ignamart03@users.noreply.github.com>
2023-06-17 11:19:48 +0200

    Update login.php

--- login.php
+++ login.php
@@ -36,6 +36,53 @@
             </li>
         </ul>
     </nav>
+
+    </nav>
+        <div class="col-sm-8">
+            <br>
+            <h2>Welcome</h2>
+            <h5>Please, log in to continue</h5>
+            <form action="profile.php" method="post">
+                <label for="usuario">Username:</label><br>
+                <input type="text" id="usuario" name="username" value=""><br>
+                <label for="pwd">Password:</label><br>
+                <input type="password" id="pwd" name="password" value=""><br><br>
+                <input type="submit" value="Submit">
+            </form>     </div>
+        <?php
+        // put your code here
+        require_once 'connect.php';
+        $username = "";
+        $email = "";
+        if (isset($_POST['Submit'])) {
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            $query = mysql_query("SELECT username FROM users WHERE username='$username' ");
+            $result2 = mysqli_query($conn, $query);
+            
+            if (mysqli_num_rows($result2) > 0) {
+                header("Location: profile.php");
+                echo"hola";
+            }
+
+
+            $sql_g = "SELECT username FROM users WHERE username='$username'";
+            $sql_s = "SELECT * FROM users WHERE email='$email'";
+            $res_g = mysqli_query($conn, $sql_g);
+            $res_s = mysqli_query($conn, $sql_s);
+
+            if (mysqli_num_rows($res_g) < 0) {
+                echo "hola";
+           
+            } else {
+                
+                echo "Saved!";
+                exit();
+            }
+        }
+        ?>
 </body>
 
 </html>
