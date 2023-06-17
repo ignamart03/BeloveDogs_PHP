816e991590b2f6aa7f86a2765a0fad87a63032c3
iorch11 <104571425+iorch11@users.noreply.github.com>
2023-06-17 13:37:38 +0200

    Shop update
    Shop code updated inserting links for every item. New items added

--- shop.php
+++ shop.php
@@ -45,14 +45,35 @@
     <div class="container mt-4">
         <h2 class="mb-4">Tienda</h2>
         <div class="row">
-            <!-- Usaremos el ciclo for para simplificar la generación de las imágenes en la grilla -->
-            <?php for ($i=1; $i <= 16; $i++): ?>
+            <!-- Bucle for para simplificar la generación de las imágenes en la página -->
+            <?php 
+            // Array de enlaces
+            $enlaces = [
+                'https://www.kiwoko.com/perros/accesorios-para-perros/juguetes-para-perros/summer-vibes-piscina-para-perros/SMV58901_M.html',
+                'https://www.kiwoko.com/perros/accesorios-para-perros/julius-k9-idc-arnes-ergonomigo-negro-para-perros/JULJ16779_M.html',
+                'https://www.kiwoko.com/peluches/fluffy-erizo-de-peluche-para-perros/FLF70448_M.html',
+                'https://www.kiwoko.com/peluches/fluffy-canvas-cocodrilo-de-peluche-para-perros/FLF16447_M.html',
+                'https://www.kiwoko.com/gatos/comida-para-gatos/maltas-y-hierbas/malta/criadores-malta-omega-3-y-6-para-gatos-y-perros/CRD32370_M.html',
+                'https://www.kiwoko.com/gatos/higiene-para-gatos/areneros-y-bandejas/nova-clean-beta-toillete-arenero-para-gatos/NCL70697_M.html',
+                'https://www.elcorteingles.es/mascotas/A18063756-pienso-para-gatos-esterilizados-senior-ultima-senior-10-pollo/?parentCategoryId=999.20999297013',
+                'https://www.tiendanimal.es/kong-snake-teaser-peluche-para-gatos/KONJU03366_M.html',
+                'https://www.miscota.es/perros/trixie/gulliver-transport-n-4-52x51x72-cm-gris-cl-gris-16100',
+                'https://www.miscota.es/perros/vectra/pr-171992',
+                'https://www.tiendanimal.es/salvaje-base-adultos-pollo-pienso-para-perros/SAB0991_M.html',
+                'https://www.kiwoko.com/perros/comida-para-perros/pienso-seco-para-perros/pienso-natural/pienso-true-origins-pure-salmon-para-perros/TRU70969_M.html',
+                'https://www.miscota.es/gatos/seresto/seresto-collar-32599',
+                'https://www.elcorteingles.es/mascotas/MP_0012933_0006R-rascador-hamiform-para-gatos-gaya/?parentCategoryId=999.20999304013&color=Blanco%20%2F%20Gris',
+                'https://www.elcorteingles.es/mascotas/A44191011-rascador-fred-and-rita-para-gatos-coche-amarillo-de-carton/?parentCategoryId=999.20999376013&color=Amarillo',
+                'https://www.bigberthaoriginal.es/products/la-cama-del-gato-cama-para-gatos-pompon-vison?currency=EUR&utm_medium=cpc&utm_source=google&utm_campaign=Google+Shopping&gclid=CjwKCAjws7WkBhBFEiwAIi16877oemGl6W-sDXTC0h4PcFK_jWRDjGInzddj9FYSLgGBLULRVV10axoCtcMQAvD_BwE&variant=15155388547115'
+            ];
+
+            for ($i = 1; $i <= 16; $i++): ?>
                 <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
                     <div class="card">
                         <img src="images/<?php echo $i; ?>.jpg" class="card-img-top" alt="Imagen <?php echo $i; ?>">
                         <div class="card-body">
                             <h5 class="card-title">Imagen <?php echo $i; ?></h5>
-                            <a href="#" class="btn btn-primary">Ir a producto</a>
+                            <a href="<?php echo $enlaces[$i - 1]; ?>" target="_blank" class="btn btn-primary">Ir a producto</a>
                         </div>
                     </div>
                 </div>
