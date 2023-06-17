e2d0d035e3ff491496e95d0fd624fc7ca84a7874
ignamart03 <90852344+ignamart03@users.noreply.github.com>
2023-06-17 12:07:54 +0200

    commit 2 styles

--- login.php
+++ login.php
@@ -5,15 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <!-- Tus Estilos -->
     <link rel="stylesheet" href="styles/base.css">
 </head>
 
 <body>
     <nav class="navbar">
-        <a href="home.php" class="nav-button" alt="Home">
-            <div style="margin-left: 1.5rem; margin-right: 1.5rem;">
-                <img src="favicon.ico">
-            </div>
+        <a class="centered-link" href="home.php" alt="Home">
+            <img src="favicon.ico">
         </a>
         <ul class="nav-list">
             <li class="nav-item">
@@ -37,58 +36,30 @@
         </ul>
     </nav>
 
-    </nav>
-    <div class="col-sm-8">
-        <br>
-        <h2>Welcome</h2>
-        <h5>Please, log in to continue</h5>
-        <form action="profile.php" method="post">
-            <label for="usuario">Username:</label><br>
-            <input type="text" id="usuario" name="username" value=""><br>
-            <label for="pwd">Password:</label><br>
-            <input type="password" id="pwd" name="password" value=""><br><br>
-            <input type="submit" value="Submit">
-        </form>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-sm-8">
+                <h2 id="title" class="mt-5">Welcome</h2>
+                <h5 id="title">Please, log in to continue</h5>
+                <form action="profile.php" method="post">
+                    <div class="form-group">
+                        <label for="usuario" id="title">Username:</label>
+                        <input type="text" id="usuario" name="username" value="" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="pwd" id="title">Password:</label>
+                        <input type="password" id="pwd" name="password" value="" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-primary mat-button">Submit</button>
+                </form>
+                <div class="mt-3">
+                    <p id="title">¿No tienes cuenta?</p>
+                    <a href="register.php" class="btn btn-secondary mat-button register-link">¡Regístrate!</a>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <div>
-        <p>¿No tienes cuenta?</p>
-        <a href="register.php" id="register">¡Regístrate!</a>
-    </div>
-
-    <?php
-    // put your code here
-    require_once 'connect.php';
-    $username = "";
-    $email = "";
-    if (isset($_POST['Submit'])) {
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-
-        $query = mysql_query("SELECT username FROM users WHERE username='$username' ");
-        $result2 = mysqli_query($conn, $query);
-
-        if (mysqli_num_rows($result2) > 0) {
-            header("Location: profile.php");
-            echo "hola";
-        }
-
-
-        $sql_g = "SELECT username FROM users WHERE username='$username'";
-        $sql_s = "SELECT * FROM users WHERE email='$email'";
-        $res_g = mysqli_query($conn, $sql_g);
-        $res_s = mysqli_query($conn, $sql_s);
-
-        if (mysqli_num_rows($res_g) < 0) {
-            echo "hola";
-        } else {
-
-            echo "Saved!";
-            exit();
-        }
-    }
-    ?>
-</body>
-
-</html>
+    <!-- Bootstrap JavaScript y jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
