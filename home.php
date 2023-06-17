ed19b4a355813acedc95f5e4ee9dd7635f033803
ignamart03 <90852344+ignamart03@users.noreply.github.com>
2023-06-17 11:44:48 +0200

    añadir carpeta styles

--- home.php
+++ home.php
@@ -2,43 +2,51 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link rel="stylesheet" href="base.css">
-    <script src="home.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link rel="stylesheet" href="styles/base.css">
+  <script src="home.js"></script>
 </head>
 
 <body>
-    <nav class="navbar">
-        <a href="home.php" class="nav-button" alt="Home">
-            <div style="margin-left: 1.5rem; margin-right: 1.5rem;">
-                <img src="favicon.ico">
-            </div>
-        </a>
-        <ul class="nav-list">
-            <li class="nav-item">
-                <a href="adopta.php" class="nav-button">Adopta</a>
-            </li>
-            <li class="nav-item">
-                <a href="login.php" class="nav-button">Login</a>
-            </li>
-            <li class="nav-item">
-                <a href="shop.php" class="nav-button">Tienda</a>
-            </li>
-            <li class="nav-item">
-                <a href="quienes.php" class="nav-button">Quienes Somos</a>
-            </li>
-            <li class="nav-item">
-                <a href="ayuda.php" class="nav-button">Ayuda</a>
-            </li>
-            <li class="nav-item">
-                <a href="otros.php" class="nav-button">Conoce Otros Dueños</a>
-            </li>
-        </ul>
-    </nav>
+  <nav class="navbar">
+    <a href="home.php" class="nav-button" alt="Home">
+      <div style="margin-left: 1.5rem; margin-right: 1.5rem;">
+        <img src="favicon.ico">
+      </div>
+    </a>
+    <ul class="nav-list">
+      <li class="nav-item">
+        <a href="adopta.php" class="nav-button">Adopta</a>
+      </li>
+      <li class="nav-item">
+        <a href="login.php" class="nav-button">Login</a>
+      </li>
+      <li class="nav-item">
+        <a href="shop.php" class="nav-button">Tienda</a>
+      </li>
+      <li class="nav-item">
+        <a href="quienes.php" class="nav-button">Quienes Somos</a>
+      </li>
+      <li class="nav-item">
+        <a href="ayuda.php" class="nav-button">Ayuda</a>
+      </li>
+      <li class="nav-item">
+        <a href="otros.php" class="nav-button">Conoce Otros Dueños</a>
+      </li>
+    </ul>
+  </nav>
 
-    <div id="carousel">
+  <div class="carrusel">
+    <img src="ruta/a/tu/imagen1.jpg" alt="Imagen 1" class="imagen-activa">
+    <img src="ruta/a/tu/imagen2.jpg" alt="Imagen 2">
+    <img src="ruta/a/tu/imagen3.jpg" alt="Imagen 3">
+    <!-- Agrega más imágenes según necesites -->
+  </div>
+
+
+  <!-- <div id="carousel">
     <section id="container-slider">
       <a href="javascript: funcionEjecutar('anterior');" class="arrowPrev"><i
           class="fas fa-chevron-circle-left"></i></a>
@@ -76,7 +84,7 @@
         </li>
       </ul>
     </section>
-  </div>
+  </div> -->
 
 </body>
 
